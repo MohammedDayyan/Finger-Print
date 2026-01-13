61d3e2fab2c64cb88a711693324bcff70e44b019
Mohammed Dayyan <dayyan.soherwardi123@gmail.com>
2026-01-13 23:47:37 +0530

    Fix Azure app structure

--- app/main.py
+++
@@ -1,5 +0,0 @@
-from ml.inference import predict_fingerprint
-
-if __name__ == "__main__":
-    result = predict_fingerprint("SOCOFing/Altered/Altered-Easy/302__M_Right_index_finger_CR.BMP")
-    print(result)
